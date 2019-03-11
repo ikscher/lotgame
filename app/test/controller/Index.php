198b13e67ddc26a7fc9eb401019309def8187ae1
ikscher <ikscher@163.com>
2019-03-11 11:17:12 +0800

    files

--- app/test/controller/Index.php
+++ app/test/controller/Index.php
@@ -8,29 +8,45 @@ class Index extends Controller
 {
     public function index()
     {
-    	// ///////////////////////////////////////////
-    	// $admin = adminModel::get(1);
-    	// $desc=$admin->admincate()->field("desc")->find();
-    	// echo $admin->nickname;
+        // echo $desc;
 
-    	// echo $desc;
-
-    	// echo $admin->admincate->desc;
+        // echo $admin->admincate->desc;
         
         ////////////////////////////////////////
         //
-     //    $admin = new adminModel();
-    	// $desc=$admin->admincate()->field("desc")->where('id','1')->find();
-    	// echo $desc;
+        //    $admin = new adminModel();
+        // $desc=$admin->admincate()->field("desc")->where('id','1')->find();
+        // echo $desc;
         //////////////////////////////////////////////
 
-        $admin_ =adminModel::get(1);
-		
+        // $admin = adminModel::hasWhere('log',['admin_menu_id'=>'50'])->find();
+        // echo $admin->name;
 
+        //一对一更新
+        // $admin = adminModel::get(1);
+        // $admin->log->ip = '192.168.1.102';
+        // // $admin->log->save();
+        // // 或者
+        // $admin->log->save(['create_time' => '23234234']);
+        //一对一插入
+        // $admin = adminModel::get(1);
+        // $admin->log()->save(['ip' => '192.168.1.103','create_time'=>'21342143']);
+        
+        //根据一对一的从表
+        // $log = AdminLog::get(3);
+        // // 输出User关联模型的属性
+        // echo $log->admin->password;
 
-		// $log->together('admin_')->save();
-		$admin_->log()->save(['admin_menu_id'=>60]);
+        //一对多
+        // $admin = adminModel::get(1);
+        // // 获取文章的所有评论
+        // $logs=collection($admin->log)->toArray();
+        // // var_dump($logs);
+        // // 也可以进行条件搜索
+        // var_dump(collection($admin->log()->where('id',4)->select())->toArray());
 
-    	return $this->fetch();
+        //模型操作
+        // var_dump(Db::name('admin_menu')->where('id','>',40)->select());
+        return $this->fetch();
     }
 }
---
+++ runtime/temp/ec2e296f2befe0366c2126ad6af17972.php
@@ -0,0 +1,2 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:1:{s:58:"D:\mywork\lotgame\public/../app/test\view\index\index.html";i:1551949388;}*/ ?>
+dsfasdfasdfasdf
