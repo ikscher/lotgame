3840fc81acd8921044a22606401b27c8dddb32b4
ikscher <ikscher@163.com>
2019-04-11 23:10:55 +0800

    files

--- runtime/temp/780f34caaf3eceda84474a22d52241b2.php
+++ runtime/temp/780f34caaf3eceda84474a22d52241b2.php
@@ -1,10 +1,11 @@
-<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:64:"D:\mywork\lotgame\public/../app/agent\view\operate\recharge.html";i:1554973536;s:49:"D:\mywork\lotgame\app\agent\view\public\left.html";i:1554865636;}*/ ?>
+<?php if (!defined('THINK_PATH')) exit(); /*a:2:{s:64:"D:\mywork\lotgame\public/../app/agent\view\operate\recharge.html";i:1554994519;s:49:"D:\mywork\lotgame\app\agent\view\public\left.html";i:1554889404;}*/ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="stylesheet" href="/static/public/layui/css/layui.css">
+	<link rel="stylesheet" href="/static/agent//css/main.css">
 	<script src="/static/public/layui/layui.js"></script>
 	<title>金币代充</title>
 </head>
@@ -68,7 +69,7 @@ layui.use(['layer','jquery','form'], function(){
     <div class="right">
     	<h1>金币代充</h1>
     	<hr>
-		<form class="layui-form" action="">
+		<form id="agentform">
 		  <div class="layui-form-item">
 		    <label class="layui-form-label">用户UID</label>
 		    <div class="layui-input-inline">
@@ -81,41 +82,52 @@ layui.use(['layer','jquery','form'], function(){
 			  <div class="layui-form-item">
 			    <label class="layui-form-label">用户昵称</label>
 			    <div class="layui-input-block">
-			      <label></label>
+			      <input type="text" id="username" disabled  class="layui-input">
 			    </div>
 			  </div>
 
 			  <div class="layui-form-item">
 			    <label class="layui-form-label">余额</label>
 			    <div class="layui-input-block">
-			      <label></label>
+			      <input type="text" id="balance" disabled class="layui-input">
 			    </div>
 			  </div>
 
 			  <div class="layui-form-item">
 			    <label class="layui-form-label">银行</label>
 			    <div class="layui-input-block">
-			      <label></label>
+			      <input type="text" id="bank" disabled class="layui-input">
 			    </div>
 			  </div>
 
 			  <div class="layui-form-item">
 			    <label class="layui-form-label">等级</label>
 			    <div class="layui-input-block">
-			      <label></label>
+			      <input type="text" id="grade" disabled  autocomplete="off" class="layui-input">
+			    </div>
+			  </div>
+
+			  <div class="layui-form-item">
+			   
+			    <div class="layui-input-block">
+			      <a class="quickcharge" href="javascript:SetQuickMoney(10)">￥10</a>
+			      <a class="quickcharge" href="javascript:SetQuickMoney(100)">￥100</a>
+			      <a class="quickcharge" href="javascript:SetQuickMoney(500)">￥500</a>
+			      <a class="quickcharge" href="javascript:SetQuickMoney(1000)">￥1000</a>
+			      <a class="quickcharge" href="javascript:SetQuickMoney(5000)">￥5000</a>
 			    </div>
 			  </div>
           </div>
 		  <div class="layui-form-item">
 		    <label class="layui-form-label">代充金额</label>
 		    <div class="layui-input-block">
-		      <input type="text" name="title" disabled  lay-verify="required" placeholder="请输入金额" autocomplete="off" class="layui-input">
+		      <input type="text" name="coins" onkeyup="value=value.replace(/[^\d\.]/ig,'')" id="coins" placeholder="请输入金额" autocomplete="off" class="layui-input">
 		    </div>
 		  </div>
 
 		  <div class="layui-form-item">
 		    <div class="layui-input-block">
-		      <button class="layui-btn layui-btn-primary" lay-submit lay-filter="formDemo">确认充值</button>
+		      <button class="layui-btn layui-btn-primary" lay-submit lay-filter="charge">确认充值</button>
 		      <!-- <button type="reset" class="layui-btn layui-btn-primary">重置</button> -->
 		    </div>
 		  </div>
@@ -132,11 +144,15 @@ layui.use(['layer','jquery','form'], function(){
                 url:"<?php echo url('agent/operate/queryuser'); ?>",
                 data:{uid:uid},
                 type:'post',
-                async: false,
                 dataType:'json',
                 success:function(res) {
+                	console.log(res)
                     if(res.code == 1) {
                         $('#userinfo').css('display','block');
+                        $('#username').val(res.username);
+                        $('#balance').val(res.balance);
+                        $('#bank').val(res.bank);
+                        $('#grade').val('V'+res.user_grade_id);
                     } else {
                     	$('#userinfo').css('display','none');
                         layer.msg(res.msg);
@@ -145,31 +161,47 @@ layui.use(['layer','jquery','form'], function(){
             })
         })
 
-        form.on('submit(reclaim)',function(){
-        	var enabled=$(this).attr('data-enabled');
-        	if(!enabled){
-                layer.msg('不可回收');
-        	    return false;
+                    
+       
+
+
+        form.on('submit(charge)',function(){
+        	var uid=$('#uid').val();
+        	if(!uid){
+        		layer.msg("请输入用户UID");
+        		return false;
         	}
-      	    $.ajax({
-                url:"<?php echo url('agent/operate/retracty'); ?>",
-                data:$('#agentform').serialize(),
-                type:'post',
-                async: false,
-                dataType:'json',
-                success:function(res) {
-                    if(res.code == 1) {
-                        // layer.alert(res.msg, function(index){
-                           location.href = res.url;
-                        // })
-                    } else {
-                        layer.msg(res.msg);
-                    }
-                }
-            })
+        	var coins=$('#coins').val();
+        	if(!coins){
+        		layer.msg('请输入金额');
+        		return false;
+        	}
+        	if(layer.confirm('确定充值麽？',function(index){
+
+        		
+	      	    $.ajax({
+	                url:"<?php echo url('agent/operate/recharge'); ?>",
+	                data:$('#agentform').serialize(),
+	                type:'post',
+	                async: false,
+	                dataType:'json',
+	                success:function(res) {
+	                    if(res.code == 1) {
+	                        
+	                    } else {
+	                        layer.msg(res.msg);
+	                    }
+	                }
+	            })
+	        }))
             return false;
         })
 
 	});
+</script>
+<script type="text/javascript">
+	function SetQuickMoney(money){
+        document.getElementById("coins").value = money;
+    }
 </script>
 </html>
